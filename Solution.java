5b92bbd89b64780f69f88e576ffbb46ac2591c44
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-08-20 20:09:07 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+	private static int numberOfCoinValues;
+	private static int targetValue;
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		numberOfCoinValues = scanner.nextInt();
+		targetValue = scanner.nextInt();
+
+		int[] coinValues = new int[numberOfCoinValues];
+		for (int value = 0; value < numberOfCoinValues; value++) {
+			coinValues[value] = scanner.nextInt();
+		}
+		scanner.close();
+
+		int result = minimumCoinsToComposeTargetValue(coinValues);
+		System.out.println(result);
+	}
+
+	private static int minimumCoinsToComposeTargetValue(int[] coinValues) {
+
+		int[] minimumCoins = new int[targetValue + 1];
+		Arrays.fill(minimumCoins, Integer.MAX_VALUE);
+		Arrays.sort(coinValues);
+		minimumCoins[0] = 0;
+
+		for (int subValue = 1; subValue <= targetValue; subValue++) {
+			for (int i = 0; i < numberOfCoinValues && coinValues[i] <= subValue; i++) {
+
+				if (minimumCoins[subValue - coinValues[i]] != Integer.MAX_VALUE) {
+					if (minimumCoins[subValue - coinValues[i]] + 1 <= minimumCoins[subValue]) {
+						minimumCoins[subValue] = minimumCoins[subValue - coinValues[i]] + 1;
+					}
+				}
+			}
+		}
+		return minimumCoins[targetValue];
+	}
+}
